b2565532de2538cf48290d24ba0d88db38f52af3
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2025-03-14 22:55:08 +0100

    Checking if typed user is human

---
+++ ContractsBot/Features/ContractsRanking/Preconditions/IsHumanUserAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace MafiaContractsBot.Features.ContractsRanking.Preconditions;
+
+public class IsHumanUserAttribute : ParameterPreconditionAttribute
+{
+    public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, IParameterInfo parameterInfo, object value, IServiceProvider services)
+    {
+        if (value is not SocketUser socketUser)
+        {
+            return Task.FromResult(PreconditionResult.FromError("Wartość parametru nie jest użytkownikiem"));
+        }
+
+        if (socketUser.IsBot)
+        {
+            return Task.FromResult(PreconditionResult.FromError("Wybrany użytkownik nie może być botem"));
+        }
+
+        return Task.FromResult(PreconditionResult.FromSuccess());
+    }
+}
